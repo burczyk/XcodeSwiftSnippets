e5b3f3f024ad434a134d39dc793ae904ada8e789
Madushan Gamage <madushanbsns@gmail.com>
2015-03-04 18:17:50 +0800

    Lazy Documents Directory Path added

---
+++ LazyDocumentDirectoryPath.swift
@@ -0,0 +1,4 @@
+lazy var applicationDocumentsDirectory: NSURL = {
+    let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+    return urls[urls.count-1] as NSURL
+    }()
